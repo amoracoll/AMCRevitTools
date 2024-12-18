5d27a3f1ca6c514e44c82c49d0cf70421ae92144
Arnau Mora Coll <143502251+amoracoll@users.noreply.github.com>
2024-12-18 12:04:23 +0100

    Add files via upload

---
+++ AMCRevitTools/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("1e2d205f-6d1a-41ea-b4d6-da896c23b7bb")]
